529ce58d643a801979e0bc7a87171590ba08b392
carmen <sogancarmen1@gmail.com>
2025-02-11 08:46:46 +0100

    modified header

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,22 @@ class App {
         credentials: true,
       })
     );
+    this.app.all("/*", function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", process.env.URL);
+      res.header(
+        "Access-Control-Allow-Methods",
+        "GET, PUT, POST, DELETE, OPTIONS"
+      );
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Content-type,Accept,X-Access-Token,X-Key"
+      );
+      if (req.method == "OPTIONS") {
+        res.status(200).end();
+      } else {
+        next();
+      }
+    });
   }
 
   private initializeControllers(controllers: Controller[]): void {
